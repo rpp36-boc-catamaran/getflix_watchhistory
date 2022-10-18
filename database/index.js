cd80283effa96db8270d6ec115d7e8af6950a04f
Shiwei Chen <shiweichenus@gmail.com>
2022-10-17 19:14:50 -0500

    Complete GET & POST requests with database

--- database/index.js
+++ database/index.js
@@ -1,11 +1,54 @@
-require('dotenv').config();
+const { Pool } = require('pg')
+require('dotenv').config()
+// console.log('process.env: ', process.env);
+
+const pool = new Pool({
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+})
 
 
-const xxx = {
+const getHistory = (userId) => {
+  return new Promise ((resolve, reject) => {
+    let queryQuestions = `
+    SELECT movie_id
+    FROM history
+    WHERE user_id = ${userId};
+    `
+    pool.query(queryQuestions, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log('here: ', result)
+        resolve(result);
+      }
+    })
+  })
 };
 
+const postHistory = (userId, movieId) => {
+  return new Promise ((resolve, reject) => {
+    let queryPostHistory = `
+    INSERT INTO history
+    (user_id, movie_id)
+    VALUES (${userId}, '${movieId}');
+    `
+    pool.query(queryPostHistory, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    })
+  })
+}
 
 
 module.exports = {
-  xxx,
+  getHistory,
+  postHistory,
 };
+
--- server/index.js
+++ server/index.js
@@ -1,14 +1,43 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const db = require('../database/index.js');
 const app = express()
-const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.get(`/profile`, (req, res) => {
+app.get(`/`, (req, res) => {
   res.send('Welcome to the profile page!')
+})
+
+app.get(`/profile`, (req, res) => {
+  let userId = 1;
+  db.getHistory(userId)
+  .then((data) => {
+    let historyList = [];
+    for (let i = 0; i < data.rows.length; i++) {
+      historyList.push(data.rows[i]['movie_id'])
+    }
+    res.status(200).send(historyList);
+  })
+  .catch((err) => {
+    res.status(400).send(err);
+  })
+})
+
+app.post('/main', (req, res) => {
+  // console.log('here: ', req.body);
+  const userId = req.body.user_id;
+  const movieId = req.body.movie_id;;
+
+  db.postHistory(userId, movieId)
+  .then(() => {
+    res.status(201).send('Success post history data');
+  })
+  .catch(err => {
+    res.status(400).send(err);
+  })
 })
 
 module.exports = app;
